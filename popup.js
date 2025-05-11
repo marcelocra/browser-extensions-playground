74e65157d29882f81e599cadff006cce75b94c7f
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-05-11 19:40:24 -0300

    feat(save opened tabs): create button to save to storage and print to output

--- popup.js
+++ popup.js
@@ -1,26 +1,79 @@
-let tabs = await chrome.tabs.query({});
+const ptBr = {
+  tabs: {
+    groups: {
+      noGroupTitle: 'Sem grupo',
+    },
+  },
+  buttons: {
+    saveOpenedTabs: 'Salvar abas abertas',
+  }
+}
 
-let map = {};
+const en = { ...ptBr }
+en.buttons.saveOpenedTabs = 'Save opened tabs';
+en.tabs.groups.noGroupTitle = 'No group';
 
-const res = tabs.map(async (tab) => {
-  let gId = tab.groupId
-  if (gId === -1) {
-    gId = 'no group';
-  } else {
-    const group = await chrome.tabGroups.get(gId);
-    gId = group.title
+const strings = { ptBr, en }
+
+document.addEventListener('DOMContentLoaded', async () => {
+  new SaveOpenedTabsButton();
+
+
+});
+
+class SaveOpenedTabsButton {
+  constructor(id = 'save-opened-tabs', lang = "ptBr") {
+    this.id = id;
+    this.t = strings[lang];
+
+    this.el = document.getElementById(id)
+    this.el.innerText = this.t.buttons.saveOpenedTabs;
+
+    this.init();
   }
 
-  return [gId, tab];
-})
-
-const groupToTab = await Promise.all(res)
-groupToTab.forEach(([gId, tab]) => {
-  if (!map[gId]) {
-    map[gId] = []
+  init() {
+    this.addEventListeners();
   }
 
-  map[gId].push(tab.url);
-})
+  addEventListeners() {
+    this.el.addEventListener('click', async () => {
+      await this.saveOpenedTabs();
+    });
+  }
 
-console.log(map);
+  async saveOpenedTabs() {
+    let tabs = await chrome.tabs.query({});
+
+    // Add group info to tab.
+    const tabWithGroupInfo = await Promise.all(tabs.map(async (tab) => {
+      let gId = tab.groupId
+      if (gId === -1) {
+        tab.group = {
+          id: -1,
+          title: this.t.tabs.groups.noGroupTitle,
+        }
+      } else {
+        const group = await chrome.tabGroups.get(gId);
+        tab.group = {
+          id: group.id,
+          title: group.title,
+        }
+      }
+
+      return tab;
+    }))
+
+    const keyedTabsToStore =
+      tabWithGroupInfo.map((tab) => {
+        return {
+          [`${tab.id}___${tab.windowId}___${tab.groupId}`]: { ...tab }
+        }
+      }).reduce((acc, curr) => {
+        return { ...acc, ...curr }
+      }, {})
+
+    console.log(keyedTabsToStore);
+    await chrome.storage.sync.set(keyedTabsToStore);
+  }
+}
